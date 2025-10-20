3298ea39fd03e0aaa0e5f8a0f8096655146e2478
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-20 21:12:58 +0500

    Antd install

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 //Router
 import { router } from './Routers/Router'
+//ANTD
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App)
 
+app.use(Antd)
 app.use(router)
 app.mount('#app')
